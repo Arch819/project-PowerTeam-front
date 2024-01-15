caf0d21d5017f15239a0876f354a50900dd2afa5
Viacheslav <gabon.qwa@gmail.com>
2024-01-15 14:30:21 +0200

    added var style, base URL for api and import style react-datepicker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-// import { Provider } from 'react-redux';
-// import { persistor, store } from 'store/store';
 import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor, store } from 'store/store';
+import { App } from 'components/App';
 // import { Loader } from 'components/Loader';
+import 'react-datepicker/dist/react-datepicker.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
