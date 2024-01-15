650676ac0b5d2c7fbb9088150050ce9ed6592f15
Viacheslav <gabon.qwa@gmail.com>
2024-01-15 18:12:37 +0200

    unused variables are commented out

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import * as statisticApi from './statisticApi';
-import * as diaryApi from './diaryApi';
+// import * as diaryApi from './diaryApi';
 
 axios.defaults.baseURL = 'http://powerpulse-backend-heie.onrender.com';
 // axios.defaults.baseURL = 'http://localhost:8000';
-const api = { statisticApi, diaryApi };
+const api = {
+  statisticApi,
+  // diaryApi
+};
 export default api;
