b6ffbcb327fc75e550d661e1217548343518e3d9
Annacheer <125766262+Annacheer@users.noreply.github.com>
2024-01-16 14:56:29 -0500

    second commit

--- src/components/Forms/AddProductForm.jsx
+++ src/components/Forms/AddProductForm.jsx
@@ -18,56 +18,7 @@
 // Якщо backend повернув помилку - необхідно її опрацювати і відобразити користувачеві у вигляді вспливаючого віконечка-notification. При цьому модальне вікно BasicModalWindow має залишатись відкритим для зручного редагування користувачем даних на випадок їх повторного
 
 function AddProductForm() {
-  return (
-    <ProductsFilterList>
-      <li>
-        <ProductsFilterLabel>
-          <ProductsFilterSearch
-            value={search}
-            onChange={onChangeSearch}
-            name="productSearch"
-            type="text"
-            placeholder="Search"
-          />
-          <ProductsBtnClose type="button" onClick={delTextInput}>
-            <ProductsSvgClose>
-              <use href={sprite + '#icon-close'}></use>
-            </ProductsSvgClose>
-          </ProductsBtnClose>
-          <ProductsBtnSearch type="button">
-            <ProductsSvgSearch>
-              <use href={sprite + '#icon-search'}></use> {/* check name */}
-            </ProductsSvgSearch>
-          </ProductsBtnSearch>
-        </ProductsFilterLabel>
-      </li>
-      <li>
-        <SelectWrapper>
-          <Select
-            styles={customStyles}
-            value={category}
-            onChange={onCategoriesChange}
-            placeholder="Categories"
-            options={categoriesList || []}
-            theme={theme => ({
-              ...theme,
-
-              colors: {
-                ...theme.colors,
-                primary50: 'rgba(255, 255, 255, 0.10)', // Цвет фона при нажатии на селект в меню
-                primary: 'transparent',
-                neutral40: '#EFEDE8', // ховер на птичку
-                neutral20: 'transparent', // дефолтный бордер
-                neutral30: 'transparent', // дефолтный ховер бордер
-                neutral50: 'rgba(239, 237, 232, 1)', // цвет плейсхолдера
-                neutral80: 'rgba(239, 237, 232, 1)',
-              },
-            })}
-          />
-        </SelectWrapper>
-      </li>
-    </ProductsFilterList>
-  );
+  return <div></div>;
 }
 
 export default AddProductForm;
--- src/components/products/ProductsFilters/index.jsx
+++ src/components/products/ProductsFilters/index.jsx
@@ -4,9 +4,138 @@
 //  - поле з випадаючим списком типів продуктів. Пошук продуктів відбувається при виборі типу
 
 // Запит на backend має відправлятися з урахуванням усіх параметрів пошуку.
+import sprite from '../../../images/sprite.svg';
+import Select from 'react-select';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import {
+  ProductsFilterLabel,
+  ProductsFilterSearch,
+  ProductsBtnClouse,
+  ProductsSvgClouse,
+  ProductsBtnSearch,
+  ProductsSvgSearch,
+  SelectWrapper,
+  ProductsFilterList,
+} from './ProductsFilter.styles';
+
+const optionsRec = [
+  { value: 'all', label: 'All' },
+  { value: 'recommended', label: 'Recommended ' },
+  { value: 'notRecommended', label: 'Not recommended' },
+];
+const categories = [
+  'alcoholic drinks',
+  'berries',
+  'cereals',
+  'dairy',
+  'dried fruits',
+  'eggs',
+  'fish',
+  'flour',
+  'fruits',
+  'meat',
+  'mushrooms',
+  'nuts',
+  'oils and fats',
+  'poppy',
+  'sausage',
+  'seeds',
+  'sesame',
+  'soft drinks',
+  'vegetables and herbs',
+];
 
 function ProductsFilters() {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  const capitalizeFirstLetter = string => {
+    const newString = string.slice(0, 1).toUpperCase() + string.slice(1);
+    return newString;
+  };
+
+  const categoriesList = categories?.map(el => ({
+    value: el,
+    label: capitalizeFirstLetter(el),
+  }));
+
+  return (
+    <div className="container">
+      <ProductsFilterList>
+        <li>
+          <ProductsFilterLabel>
+            <ProductsFilterSearch
+              value={search}
+              onChange={onChangeSearch}
+              name="productSearch"
+              type="text"
+              placeholder="Search"
+            />
+            <ProductsBtnClose type="button" onClick={delTextInput}>
+              <ProductsSvgClose>
+                <use href={sprite + '#icon-close'}></use>
+              </ProductsSvgClose>
+            </ProductsBtnClose>
+            <ProductsBtnSearch type="button">
+              <ProductsSvgSearch>
+                <use href={sprite + '#icon-search'}></use>
+              </ProductsSvgSearch>
+            </ProductsBtnSearch>
+          </ProductsFilterLabel>
+        </li>
+        <li>
+          <SelectWrapper>
+            <Select
+              styles={customStyles}
+              value={category}
+              onChange={onCategoriesChange}
+              placeholder="Categories"
+              options={categoriesList || []}
+              theme={theme => ({
+                ...theme,
+
+                colors: {
+                  ...theme.colors,
+                  primary50: 'rgba(255, 255, 255, 0.10)', // Цвет фона при нажатии на селект в меню
+                  primary: 'transparent',
+                  neutral40: '#EFEDE8', // ховер на птичку
+                  neutral20: 'transparent', // дефолтный бордер
+                  neutral30: 'transparent', // дефолтный ховер бордер
+                  neutral50: 'rgba(239, 237, 232, 1)', // цвет плейсхолдера
+                  neutral80: 'rgba(239, 237, 232, 1)',
+                },
+              })}
+            />
+          </SelectWrapper>
+        </li>
+        <li>
+          <SelectWrapper>
+            <Select
+              styles={customStyles}
+              onChange={onRecomendedChange}
+              value={recommended}
+              theme={theme => ({
+                ...theme,
+
+                colors: {
+                  ...theme.colors,
+                  primary50: 'rgba(255, 255, 255, 0.10)', // Цвет фона при нажатии на селект в меню
+                  primary: 'transparent',
+                  neutral40: '#EFEDE8', // ховер на птичку
+                  neutral20: 'transparent', // дефолтный бордер
+                  neutral30: 'transparent', // дефолтный ховер бордер
+                  neutral50: 'rgba(239, 237, 232, 1)', // цвет плейсхолдера
+                  neutral80: 'rgba(239, 237, 232, 1)',
+                },
+              })}
+              options={optionsRec}
+            />
+          </SelectWrapper>
+        </li>
+      </ProductsFilterList>
+    </div>
+  );
 }
 
 export default ProductsFilters;
