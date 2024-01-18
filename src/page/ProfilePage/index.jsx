0789d44aff6654a43b33a841fbf4eeaec67daea8
Serhiy8 <markovsm86@gmail.com>
2024-01-18 16:59:48 +0200

    UserForm

--- src/page/ProfilePage/index.jsx
+++ src/page/ProfilePage/index.jsx
@@ -5,10 +5,18 @@
 //  - компоненту UserCard, що містить інформацію про користувача
 // Увімкнути програму зчитування з екрана
 
+import UserForm from 'components/Forms/UserForm';
+import { PageProfileContainer } from './PageProfile.styled';
+
 // Містить заоголовок сторінки та має бути реалізований, як універсальний компонент, що можна використати на інших сторінках застосунку
 
 const ProfilePage = () => {
-  return <h2>Profile Settings</h2>;
+  return (
+    <PageProfileContainer className="container">
+      <h2>Profile Settings</h2>
+      <UserForm />
+    </PageProfileContainer>
+  );
 };
 
 export default ProfilePage;
