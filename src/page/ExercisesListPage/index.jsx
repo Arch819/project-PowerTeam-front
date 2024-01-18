904084bf9e42bf82f97b46c6b515846a762086ad
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-01-18 14:24:16 +0200

    render and styles added

--- src/page/ExercisesListPage/index.jsx
+++ src/page/ExercisesListPage/index.jsx
@@ -1,10 +1,84 @@
 import Section from 'components/Section';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import sprite from '../../images/sprite.svg';
+import ExercisesList from 'components/Exercises/ExercisesList';
+import ExercisesCategories from 'components/Exercises/ExercisesCategories';
+import { Icon, LinkBack, TitleBox } from './ExercisesListPage.styled';
+import { useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Oval } from 'react-loader-spinner';
+
+axios.defaults.baseURL = 'https://powerpulse-backend-heie.onrender.com/';
 
 function ExercisesListPage() {
+  const [exercisesArray, setExercisesArray] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { category, subcategory } = useParams();
+  console.log(category, subcategory);
+
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/exercises';
+  const ref = useRef(backLinkHref);
+
+  const getExercisesArray = useCallback(async () => {
+    try {
+      // запит на бек
+      setLoading(true);
+      const response = await axios.get('/exercises');
+
+      const filterValid = {
+        bodyParts: 'bodyPart',
+        muscles: 'target',
+        equipment: 'equipment',
+      };
+      const categoryValid = filterValid[category];
+
+      const arrayToRender = response.data.filter(
+        item => item[categoryValid].replaceAll(' ', '') === subcategory
+      );
+      setExercisesArray(arrayToRender);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [category, subcategory]);
+
+  useEffect(() => {
+    getExercisesArray();
+  }, [getExercisesArray]);
+
   return (
     <Section use={'first'}>
       <div className="container">
-        <h2>Waist</h2>
+        <LinkBack to={ref.current}>
+          <Icon>
+            <use href={`${sprite}#${'icon-back'}`} />
+          </Icon>
+          Back
+        </LinkBack>
+        <TitleBox>
+          {/*  <TitlePage title={'Waist'} /> */}
+          <h3>Waist</h3>
+          <ExercisesCategories activeCategory={category} />
+        </TitleBox>
+        {loading ? (
+          <Oval
+            visible={true}
+            height="80"
+            width="80"
+            color="#E6533C"
+            ariaLabel="oval-loading"
+            wrapperStyle={{
+              display: 'block',
+              marginRight: 'auto',
+              marginLeft: 'auto',
+            }}
+            wrapperClass=""
+          />
+        ) : (
+          <ExercisesList exercisesArray={exercisesArray} />
+        )}
       </div>
     </Section>
   );
--- src/page/ExercisesPage/index.jsx
+++ src/page/ExercisesPage/index.jsx
@@ -1,18 +1,93 @@
-// Exercises page "/exercises"
-// Складається з:
-//  - компоненту TitlePage (опис компонента деталізовано у Diary page)
-//  - компоненту ExercisesCategories, що дозволяє користувачу обрати категорію вправ, за якою він буде фільтрувати вправи
-//  - компоненту ExercisesSubcategoriesList, що містить перелік підкатегорій вправ ExercisesSubcategoriesItem, в залежності від
-// обраної користувачем категорії
-//   - компоненту ExercisesList, що містить перелік вправ і відображається у разі обрання певної ExercisesSubcategoriesItem
-
 import Section from 'components/Section';
+import ExercisesCategories from 'components/Exercises/ExercisesCategories';
+import ExercisesSubcategoriesList from 'components/Exercises/ExercisesSubcategoriesList';
+import TitlePage from 'components/TitlePage';
+import { NavBox } from './ExercisesPage.styled';
+import { useParams } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { Oval } from 'react-loader-spinner';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://powerpulse-backend-heie.onrender.com/';
 
 function ExercisesPage() {
+  const [subcategoriesList, setSubcategoriesList] = useState([]);
+  const [limit, setLimit] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const { category } = useParams();
+
+  const handleResize = () => {
+    const windowWidth = window.innerWidth;
+    if (windowWidth >= 1440 || windowWidth < 768) {
+      setLimit(10);
+    } else {
+      setLimit(9);
+    }
+  };
+  useEffect(() => {
+    handleResize();
+  }, []);
+
+  window.addEventListener('resize', handleResize);
+
+  const getSubcategoriesList = useCallback(
+    async query => {
+      try {
+        setLoading(true);
+        // запрос на бек
+        const response = await axios.get('/exercises/filters');
+
+        const filterValid = {
+          bodyParts: 'Body parts',
+          muscles: 'Muscles',
+          equipment: 'Equipment',
+        };
+        const subCategoriesToRender = response.data.filter(
+          item => item.filter === filterValid[category]
+        );
+        setSubcategoriesList(subCategoriesToRender);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [category]
+  );
+
+  useEffect(() => {
+    getSubcategoriesList();
+  }, [getSubcategoriesList]);
+
   return (
     <Section>
       <div className="container">
-        <h2>Exercises</h2>
+        <NavBox>
+          {/*  <TitlePage title={'Exercises'} /> */}
+          <h3>Exercises</h3>
+          <ExercisesCategories activeCategory={category} />
+        </NavBox>
+
+        {loading ? (
+          <Oval
+            visible={true}
+            height="80"
+            width="80"
+            color="#E6533C"
+            ariaLabel="oval-loading"
+            wrapperStyle={{
+              display: 'block',
+              marginRight: 'auto',
+              marginLeft: 'auto',
+            }}
+            wrapperClass=""
+          />
+        ) : (
+          <ExercisesSubcategoriesList
+            subcategoriesList={subcategoriesList}
+            category={category}
+          />
+        )}
       </div>
     </Section>
   );
