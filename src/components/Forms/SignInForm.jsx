8cc2a1826bd3b2c7d37032939cadcdde94088ae4
Maxim Sytnik <118841889+Maksim713@users.noreply.github.com>
2024-01-18 18:58:18 +0200

    add sign up and sign in form

--- src/components/Forms/SignInForm.jsx
+++ src/components/Forms/SignInForm.jsx
@@ -1,12 +1,136 @@
-// SignInForm містить інпути (Email, Password), кнопку типу "submit" - Sign In та має бути провалідована по події submit за допомогою бібліотек Formik + Yup.
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import {
+  Form,
+  StyledInputContainer,
+  StyledLabel,
+  StyledInput,
+  IconWrapper,
+  SvgIcon,
+  ErrorMessage,
+  StyledPasswordInputContainer,
+  IconPasswordWrapper,
+  SvgPasswordIcon,
+  SubmitButton,
+} from './SignForm.styled';
+import sprite from 'images/sprite.svg';
+import { useState } from 'react';
 
-// email - string with pattern /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ , required
-// password - string, min 6, required
+const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-// У разі наявності невалідних значень, причина помилки повинна бути відображена користувачеві, а дані - не відправлятися на backend.
-// У разі, якщо всі значення валідні, - дані слід відправити на backend.
+const ValidationIcon = ({ error, touched, successText, errorText }) => {
+  return (
+    <IconWrapper>
+      <SvgIcon fill={!error && touched ? '#3CBF61' : null}>
+        <use href={`${sprite}#icon-checkmark`} />
+      </SvgIcon>
+      <ErrorMessage color={!error && touched ? '#3CBF61' : null}>
+        {!error ? successText : errorText}
+      </ErrorMessage>
+    </IconWrapper>
+  );
+};
 
-// Якщо backend повернув помилку - необхідно її опрацювати і відобразити користувачеві у вигляді вспливаючого віконечка-notification.
-// Якщо запит на backend пройшов успішно та token для користувача сформовано - необхідно реалізувати авторизацію та переадресувати його на приватну сторінку:
-//  - Profile page - якщо на backendі відсутня інформація про параметри авторизованого користувача
-//  - Diary page - якщо на backendі наявна інформація про параметри авторизованого користувача
+const SignInForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: values => {
+      console.log('submit', values);
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .matches(emailPattern, 'Doesn`t look like a valid email')
+        .required('Please enter your email address'),
+      password: Yup.string().min(6).required('Please enter your password'),
+    }),
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(prev => !prev);
+  };
+
+  const passwordIcon = showPassword ? 'icon-eye' : 'icon-eye-off';
+
+  return (
+    <form className={Form} onSubmit={formik.handleSubmit}>
+      <StyledInputContainer>
+        <StyledLabel
+          error={formik.errors.email && formik.touched.email}
+          active={formik.touched.email || formik.values.email !== ''}
+        >
+          Email
+        </StyledLabel>
+        <StyledInput
+          name="email"
+          type="email"
+          placeholder=""
+          error={formik.errors.email && formik.touched.email}
+          success={
+            !formik.errors.email && formik.touched.email && formik.values.email
+          }
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+
+        {(formik.errors.email && formik.touched.email) ||
+        (!formik.errors.email && formik.touched.email) ? (
+          <ValidationIcon
+            error={formik.errors.email}
+            touched={formik.touched.email}
+            errorText={formik.errors.email}
+            successText={'Success email'}
+          />
+        ) : null}
+      </StyledInputContainer>
+
+      <StyledPasswordInputContainer>
+        <StyledLabel
+          error={formik.errors.password && formik.touched.password}
+          active={formik.touched.password || formik.values.password !== ''}
+        >
+          Password
+        </StyledLabel>
+        <StyledInput
+          name="password"
+          type={showPassword ? 'text' : 'password'}
+          placeholder=""
+          error={formik.errors.password && formik.touched.password}
+          success={
+            !formik.errors.password &&
+            formik.touched.password &&
+            formik.values.password
+          }
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+
+        <IconPasswordWrapper onClick={togglePassword}>
+          <SvgPasswordIcon>
+            <use href={`${sprite}#${passwordIcon}`} />
+          </SvgPasswordIcon>
+        </IconPasswordWrapper>
+      </StyledPasswordInputContainer>
+
+      {(formik.errors.password && formik.touched.password) ||
+      (!formik.errors.password && formik.touched.password) ? (
+        <ValidationIcon
+          error={formik.errors.password}
+          touched={formik.touched.password}
+          errorText={formik.errors.password}
+          successText={'Success password'}
+        />
+      ) : null}
+
+      <SubmitButton type="submit">Sign In</SubmitButton>
+    </form>
+  );
+};
+
+export default SignInForm;
--- src/page/SignInPage/index.jsx
+++ src/page/SignInPage/index.jsx
@@ -1,6 +1,7 @@
 // SignIn page "/signin"
 // Складається з SignInForm та посилання на сторінку SignUp page
 
+import SignInForm from 'components/Forms/SignInForm';
 import Section from 'components/Section';
 
 const SignInPage = () => {
@@ -8,6 +9,7 @@ const SignInPage = () => {
     <Section use={'first'}>
       <div className="container">
         <h2>Sign In</h2>
+        <SignInForm />
       </div>
     </Section>
   );
