82dd0684d9eb8857346ff624569a94a29fca2113
Igor9779 <126416571+Igor9779@users.noreply.github.com>
2024-01-19 17:33:34 +0200

    products-success-&-exercise-success

--- src/components/Modal/AddProductSuccess.jsx
+++ src/components/Modal/AddProductSuccess.jsx
@@ -3,8 +3,45 @@
 //  - кнопку Next product, по clickу на яку BasicModalWindow з AddProductSuccess має бути закрито
 //  - посилання To the diary, по clickу на яке користувача має бути переадресовано на Diary page
 
-function AddProductSuccess() {
-  return <div></div>;
-}
+import { Link } from 'react-router-dom';
+import avocado from '../../images/avocado-2x.png';
+import sprite from '../../images/sprite.svg';
+import {
+  SuccessModalWindow,
+  SuccessModalWindowWrap,
+  SuccessModalWindowWrapImg,
+  SuccessModalWindowImg,
+  SuccessModalWindowTitle,
+  SuccessModalWindowText,
+  SuccessModalWindowSpan,
+  SuccessModalWindowBtn,
+  ArrowIcon,
+} from './AddProductSuccess.styles';
 
-export default AddProductSuccess;
+export const AddProductSuccess = ({ calories, closeModal }) => {
+  return (
+    <SuccessModalWindow>
+      <SuccessModalWindowWrap>
+        <SuccessModalWindowWrapImg>
+          <SuccessModalWindowImg src={avocado} alt="avocado" />
+          <SuccessModalWindowTitle>Well done</SuccessModalWindowTitle>
+          <SuccessModalWindowText>
+            Calories:{' '}
+            <SuccessModalWindowSpan>{calories}</SuccessModalWindowSpan>
+          </SuccessModalWindowText>
+        </SuccessModalWindowWrapImg>
+        <Link to="/products" onClick={closeModal}>
+          <SuccessModalWindowBtn>Next product</SuccessModalWindowBtn>
+        </Link>
+        <Link to="/diary" onClick={closeModal}>
+          <SuccessModalWindowText>
+            To the diary
+            <ArrowIcon>
+              <use href={`${sprite}#icon-next`}> </use>
+            </ArrowIcon>
+          </SuccessModalWindowText>
+        </Link>
+      </SuccessModalWindowWrap>
+    </SuccessModalWindow>
+  );
+};
