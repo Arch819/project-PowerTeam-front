4d84453b81c3b7c2a88b0363817d4fc9f3d7e764
Viacheslav <116117866+Arch819@users.noreply.github.com>
2024-01-19 23:17:12 +0200

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
-// import persistReducer from 'redux-persist/es/persistReducer';
-// import storage from 'redux-persist/lib/storage';
+ import persistReducer from 'redux-persist/es/persistReducer';
+ import storage from 'redux-persist/lib/storage';
 import { appReducer } from './appState/slice';
 import { authReducer } from './auth/slice';
 import { diaryReducer } from './diary/diarySlice';
@@ -7,15 +7,15 @@ import { exercisesReducer } from './exercises/exercisesSlice';
 import { productsReducer } from './products/productsSlice';
 import { statisticReducer } from './statistic/statisticSlice';
 
-// const persistConfig = {
-//   key: 'user',
-//   storage,
-//   whitelist: ['token'],
-// };
-// const userPersistReducer = persistReducer(persistConfig, );
+ const persistConfig = {
+   key: 'user',
+   storage,
+   whitelist: ['token'],
+ };
+ const userPersistReducer = persistReducer(persistConfig, authReducer );
 
 export const reducer = {
-  auth: authReducer,
+  auth: userPersistReducer,
   products: productsReducer,
   exercises: exercisesReducer,
   diary: diaryReducer,
