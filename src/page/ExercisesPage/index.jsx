eb30fe8fb7053772b3927cb9211fbc29e57d3da3
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-01-19 16:44:19 +0200

    save

--- src/page/ExercisesPage/index.jsx
+++ src/page/ExercisesPage/index.jsx
@@ -3,7 +3,7 @@ import ExercisesCategories from 'components/Exercises/ExercisesCategories';
 import ExercisesSubcategoriesList from 'components/Exercises/ExercisesSubcategoriesList';
 import TitlePage from 'components/TitlePage';
 import { NavBox } from './ExercisesPage.styled';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useCallback, useEffect, useState } from 'react';
 import { Oval } from 'react-loader-spinner';
 import axios from 'axios';
@@ -13,51 +13,53 @@ function ExercisesPage() {
   const [subcategoriesList, setSubcategoriesList] = useState([]);
   const [limit, setLimit] = useState(0);
   const [loading, setLoading] = useState(false);
-
+  const navigate = useNavigate();
   const { category } = useParams();
 
-  const handleResize = () => {
-    const windowWidth = window.innerWidth;
-    if (windowWidth >= 1440 || windowWidth < 768) {
-      setLimit(10);
-    } else {
-      setLimit(9);
+  // const handleResize = () => {
+  //   const windowWidth = window.innerWidth;
+  //   if (windowWidth >= 1440 || windowWidth < 768) {
+  //     setLimit(10);
+  //   } else {
+  //     setLimit(9);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   handleResize();
+  // }, []);
+
+  // window.addEventListener('resize', handleResize);
+
+  const getSubcategoriesList = useCallback(async category => {
+    try {
+      setLoading(true);
+      // запрос на бек
+      const response = await axios.get('/exercises/filters');
+
+      const filterValid = {
+        bodyPart: 'Body parts',
+        target: 'Muscles',
+        equipment: 'Equipment',
+      };
+      const subCategoriesToRender = response.data.filter(
+        item => item.filter === filterValid[category]
+      );
+      setSubcategoriesList(subCategoriesToRender);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
     }
-  };
-  useEffect(() => {
-    handleResize();
   }, []);
 
-  window.addEventListener('resize', handleResize);
-
-  const getSubcategoriesList = useCallback(
-    async query => {
-      try {
-        setLoading(true);
-        // запрос на бек
-        const response = await axios.get('/exercises/filters');
-
-        const filterValid = {
-          bodyPart: 'Body parts',
-          target: 'Muscles',
-          equipment: 'Equipment',
-        };
-        const subCategoriesToRender = response.data.filter(
-          item => item.filter === filterValid[category]
-        );
-        setSubcategoriesList(subCategoriesToRender);
-      } catch (error) {
-        console.log(error.message);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [category]
-  );
-
   useEffect(() => {
-    getSubcategoriesList();
-  }, [getSubcategoriesList]);
+    if (!category) {
+      navigate('/exercises/bodyPart', { replace: true });
+      return;
+    }
+    getSubcategoriesList(category);
+  }, [getSubcategoriesList, category, navigate]);
 
   return (
     <Section>
