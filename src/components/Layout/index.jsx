f4960e4b45b8f05071cba572bf7366db37fe1edb
Viacheslav <gabon.qwa@gmail.com>
2024-01-20 22:50:13 +0200

    added global loading output

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,16 +4,17 @@ import { notiflixMessage } from 'helpers/notiflixMessage';
 import { Suspense, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
-import { selectError } from 'store/appState/selectors';
+import { selectError, selectIsLoading } from 'store/appState/selectors';
 
 const Layout = () => {
+  const loading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   useEffect(() => {
     error && notiflixMessage('error', error);
   });
   return (
     <>
-      {/* {loading && <Loader />} */}
+      {loading && <Loader />}
       <Header />
       <main>
         <Suspense fallback={<Loader />}>
