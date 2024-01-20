538f517e1a4a27dc8c48a7eb3cda8d56238517db
Viacheslav <gabon.qwa@gmail.com>
2024-01-20 22:47:11 +0200

    added global error output

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,16 @@
 import Header from 'components/Header';
 import { Loader } from 'components/Loader';
-import { Suspense } from 'react';
+import { notiflixMessage } from 'helpers/notiflixMessage';
+import { Suspense, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { selectError } from 'store/appState/selectors';
 
 const Layout = () => {
+  const error = useSelector(selectError);
+  useEffect(() => {
+    error && notiflixMessage('error', error);
+  });
   return (
     <>
       {/* {loading && <Loader />} */}
