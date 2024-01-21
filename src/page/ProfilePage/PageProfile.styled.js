6c5de0653d7e9a063b187b1f7c7aa48a76765771
Serhiy8 <markovsm86@gmail.com>
2024-01-21 16:21:06 +0200

    title added

--- src/page/ProfilePage/PageProfile.styled.js
+++ src/page/ProfilePage/PageProfile.styled.js
@@ -1,22 +1,9 @@
 import styled from 'styled-components';
 
 export const PageProfileContainer = styled.div`
-  padding-top: 100px;
+  padding-top: 40px;
   @media (min-width: 768px) {
-    padding-top: 156px;
-  }
-`;
-
-export const ProfileTitle = styled.h2`
-  color: var(--color-text-primary);
-  font-family: Roboto;
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 28px;
-  @media (min-width: 768px) {
-    font-size: 32px;
-    line-height: 44px;
+    padding-top: 72px;
   }
 `;
 
