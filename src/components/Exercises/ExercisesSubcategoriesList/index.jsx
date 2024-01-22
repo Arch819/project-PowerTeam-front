48f9520bc5bd968b503e10f02f67331ba7622bfb
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-01-22 11:23:27 +0200

    swiper first varint

--- src/components/Exercises/ExercisesSubcategoriesList/index.jsx
+++ src/components/Exercises/ExercisesSubcategoriesList/index.jsx
@@ -1,21 +1,72 @@
-import ExercisesSubcategoriesItem from '../ExercisesSubcategoriesItem';
-import { SubCategoriesList } from './ExercisesSubcategoriesList.styled';
+import React from 'react';
+import {
+  Image,
+  SwiperBox,
+  SwiperItem,
+  TextContainer,
+} from './ExercisesSubcategoriesList.styled';
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+import { Grid, Pagination } from 'swiper/modules';
+import { Link, useLocation } from 'react-router-dom';
 
 const ExercisesSubcategoriesList = ({ subcategoriesList, category }) => {
+  const location = useLocation();
+
+  const capitalizedWord = word => {
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
+  };
+
   return (
-    <div>
-      <SubCategoriesList>
+    <>
+      <SwiperBox
+        slidesPerView={1}
+        grid={{
+          fill: 'row',
+          rows: 10,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          768: {
+            slidesPerView: 3,
+            grid: {
+              fill: 'row',
+              rows: 3,
+            },
+            spaceBetween: 16,
+          },
+          1440: {
+            slidesPerView: 5,
+            grid: {
+              fill: 'row',
+              rows: 2,
+            },
+            spaceBetween: 16,
+          },
+        }}
+        modules={[Grid, Pagination]}
+        className="mySwiper"
+      >
         {subcategoriesList.map(({ idFilter, filter, name, imgURL }) => (
-          <ExercisesSubcategoriesItem
-            key={idFilter}
-            filter={filter}
-            name={name}
-            imgURL={imgURL}
-            category={category}
-          />
+          <SwiperItem key={idFilter}>
+            <Link
+              to={`/exercises/${category}/${encodeURIComponent(name)}`}
+              state={{ from: location }}
+            >
+              <Image src={imgURL} alt="name"></Image>
+              <TextContainer>
+                <h3>{capitalizedWord(name)}</h3>
+                <p>{filter}</p>
+              </TextContainer>
+            </Link>
+          </SwiperItem>
         ))}
-      </SubCategoriesList>
-    </div>
+      </SwiperBox>
+    </>
   );
 };
 
