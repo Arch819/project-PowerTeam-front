9e519279cfbc3ec833af7bf19548a1b129b728b5
Alexa01821 <sahunia2004@gmail.com>
2024-01-22 18:34:04 +0200

    Update focus&hover

--- src/components/Exercises/ExercisesItem/index.jsx
+++ src/components/Exercises/ExercisesItem/index.jsx
@@ -1,10 +1,3 @@
-// ExercisesItem містить інформацію про вправу, а саме:
-//  - назву вправи
-//  - кількість калорій, яку буде спалено за 60 хвилин виконання вправи
-//  - частину тіла, яка буде задіяна під час виконання вправи
-//  - мʼязи, які буде вдосконалено в результаті виконання вправи
-// Також ExercisesItem містить кнопку Start, що відкриває модальне вікно BasicModalWindow,  яке містить AddExerciseForm
-
 import { useState } from 'react';
 import ExercisesItemStyled from './ExercisesItemStyled';
 import sprite from '../../../images/sprite.svg';
